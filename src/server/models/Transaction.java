2a5dac53ea105390aca76ff2b2b6fc2f51ff501d
Kyle <kkumar.75511@farnborough.ac.uk>
2018-07-13 23:52:34 +0100

    Storing transaction and nextID
    Created an arrayList to store every transaction as well as a method that works out what the next unique ID is by going through all transactions stored in the arrayList.

--- src/server/models/Transaction.java
+++ src/server/models/Transaction.java
@@ -1,11 +1,17 @@
 package server.models;
 
+import java.util.ArrayList;
+
 public class Transaction {
 
     private int transactionID;
     private float transactionAmount;
     private String transactionCategory;
     private String transactionDate;
+
+    // Created an arrayList that will store every transaction
+    // The arrayList is static so that only one instance of the arrayList is created
+    public static ArrayList<Transaction> transactions = new ArrayList<Transaction>();
 
     // Constructor
     public Transaction (int transactionID, float transactionAmount, String transactionCategory, String transactionDate) {
@@ -14,6 +20,37 @@ public class Transaction {
         this.transactionAmount = transactionAmount;
         this.transactionCategory = transactionCategory;
         this.transactionDate = transactionDate;
+
+    }
+
+    // Function that works out what the next unused id is by going through all the transactions in the arrayList
+    public static int nextId () {
+
+        int id = 0;
+
+        for (Transaction transaction : transactions) {
+
+            if (transaction.getTransactionID() > id) {
+
+                id = transaction.getTransactionID();
+
+            }
+
+        }
+
+        return id + 1;
+
+    }
+
+    @Override
+    public String toString () {
+
+        return "Transaction{" +
+                "transactionID=" + transactionID +
+                ", transactionAmount=" + transactionAmount +
+                ", transactionCategory='" + transactionCategory + '\'' +
+                ", transactionDate='" + transactionDate + '\'' +
+                '}';
 
     }
 
@@ -32,17 +69,5 @@ public class Transaction {
     public String getTransactionDate () { return transactionDate; }
 
     public void setTransactionDate (String transactionDate) { this.transactionDate = transactionDate; }
-
-    @Override
-    public String toString () {
-
-        return "Transaction{" +
-                "transactionID=" + transactionID +
-                ", transactionAmount=" + transactionAmount +
-                ", transactionCategory='" + transactionCategory + '\'' +
-                ", transactionDate='" + transactionDate + '\'' +
-                '}';
-
-    }
 
 }

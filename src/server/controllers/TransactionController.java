9c6c3d21951acff29c05100316c25466983bd99a
Kyle <kkumar.75511@farnborough.ac.uk>
2018-07-14 00:05:55 +0100

    New transaction HTTP post handler
    Re creating the HTTP post handler so that a unique transactionID is created and added to the transaction arrayList

--- src/server/controllers/TransactionController.java
+++ src/server/controllers/TransactionController.java
@@ -1,25 +1,30 @@
 package server.controllers;
 
-import server.Console;
+import server.models.Transaction;
 
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 
-@Path("/transaction")
+@Path("transaction/")
 public class TransactionController {
 
     @POST
     @Path("new")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Produces(MediaType.TEXT_PLAIN)
-    public String transactionHandler (@FormParam("transactionAmount") String transactionAmount,
+    public String transactionHandler (@FormParam("transactionAmount") float transactionAmount,
                                       @FormParam("transactionCategory[]") String transactionCategory,
                                       @FormParam("transactionDate") String transactionDate) {
 
-        Console.log("Transaction: £" + transactionAmount + " , " + transactionCategory + " , " + transactionDate);
-        return "Transaction of " + transactionAmount + " pounds added to the " + transactionCategory + " category on " + transactionDate + ".";
+        int transactionId = Transaction.nextId();
+
+        Transaction.transactions.add(new Transaction(transactionId, transactionAmount, transactionCategory, transactionDate));
+
+        return "TransactionID: " + transactionId + " | TransactionAmount: " + transactionAmount + " | TransactionCategory: " + transactionCategory + " | TransactionDate: " + transactionDate;
 
     }
 
